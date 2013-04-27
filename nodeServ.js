328c01cf038361c928a384ad40f2f6b3bcb06ce1
Jade Lacosse <sprucegum@gmail.com>
2013-04-26 17:20:27 -0700

    used 'static-resource' to enable loading of external resources

--- nodeServ.js
+++ nodeServ.js
@@ -1,54 +1,39 @@
+/*
+	JTK server backendy
+*/
+
 
 var http = require('http'),
     fs = require('fs'),
     url = require('url'),
     index;
+var staticResource = require('static-resource');
 
-fs.readFile('./cursor.png', function (err, data) {
-    if (err) {
-        throw err;
-    }
-    cursorimg = data;
-});
+var handler = staticResource.createHandler(fs.realpathSync('./'));
+//handler.addContentType('.ttf', 'application/octet-stream');
 
-fs.readFile('./Display.html', function (err, data) {
-    if (err) {
-        throw err;
-    }
-    index = data;
-});
-fs.readFile('./jTK.js', function(err, data) {
-    if (err) {
-        throw err;
-    }
-    jTK = data;
-}); 
 
 http.createServer(function (req, res) {
   var qParsed = url.parse(req.url, true);
   var qData = qParsed.query;
   var qPath = qParsed.pathname;
-  console.log(qPath);
-  console.log(qData);
-
-  if (qPath.match('jTK.js')){
-	res.writeHead(200, {'Content-Type': 'text/javascript'});
-	res.write(jTK);
-
-  } else if (qPath.match('cursor.png')){
-    res.writeHead(200, {'Content-Type': 'image/png'});
-    res.write(cursorimg);
-
-  } else if (qData.jtkConnection){	
-	  res.writeHead(200, {'Content-Type': 'text/json'});
-	  rJSON = JSON.stringify(["Test Title", qPath, qData]);
-	  res.write(rJSON);
-  } else {
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.write(index);  
+  //console.log(qPath);
+  //console.log(qData);
+  //console.log(req, res);
+  var path = url.parse(req.url).pathname;
+  if(!handler.handle(path, req, res)) {
+	console.log(qPath);
+	res.writeHead(404);
+	res.write('404');
+	res.end();
+  } else if (qData.jtkConnection){
+	console.log("JSON");	
+	res.writeHead(200, {'Content-Type': 'text/json'});
+	rJSON = JSON.stringify(["Test Title", qPath, qData]);
+	res.write(rJSON);
   }
   res.end('');
   
-}).listen(1337, '127.0.0.1');
+}).listen(1337, '192.168.0.29');
 
 console.log('Server running at http://127.0.0.1:1337/');
