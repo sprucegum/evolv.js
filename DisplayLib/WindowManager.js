e6b0656e2108234856440a940aec8dd28378f0ff
Jade Lacosse <sprucegum@gmail.com>
2013-04-27 20:44:18 -0700

    Implemented server side window spawning and client side layout engine.

--- DisplayLib/WindowManager.js
+++ DisplayLib/WindowManager.js
@@ -20,13 +20,14 @@ ConnectionManager = function(serverRoot){
 
 };
 
-ConnectionManager.prototype.returnJSON = function(jsonStr){
-	return JSON.parse(json);
+ConnectionManager.prototype.returnJSON = function(ajaxEvent){
+	//console.log("jsonString",ajaxEvent);
+	return JSON.parse(ajaxEvent.currentTarget.response);
 };
 
 ConnectionManager.prototype.getJSON = function(jsonObj, callback){
 	var rq = new XMLHttpRequest();
-	rq.open("GET", this.serverRoot + JSON.stringify(jsonObj), 1);
+	rq.open("GET", this.serverRoot + "?jtk=true&data=" + JSON.stringify({jtkConnection:true,data:jsonObj}), 1);
 	rq.onloadend = callback;
 	rq.send();
 };
@@ -41,16 +42,33 @@ ConnectionManager.prototype.postJSON = function(jsonObj, callback){
 // Takes a list of windows and sets the dimensions of each window 
 // 
 
-WindowManager = function (canvas, jsonList) {
+WindowManager = function (canvas) {
 	this.canvas = canvas;
 	this.width = canvas.width;
 	this.height = canvas.height;
-	this.jsonList = jsonList;
-	this.padding = 10;
-	this.conMan = new ConnectionManager();
+	this.padding = 50;
+	this.conMan = new ConnectionManager('/');
+	
+}
+WindowManager.prototype.start = function(){
+	var winset = this.getWindowSet("main_page");
+};
+
+WindowManager.prototype.getWindowSet = function(set_name){
+	//this.conMan.getJSON	
+	var request_object = {command:"get_set",set:set_name};
+	var winman = this;
+	return this.conMan.getJSON(request_object,function (resp){
+		winman.buildWindows(winman.conMan.returnJSON(resp))
+	});
+}
+
+WindowManager.prototype.buildWindows = function(jsonList){
 	var win = null;
 	var cursor = [0,0]; // x, y
 	var cellsize = [0,0]; // width, height
+	var canvas = this.canvas;
+	console.log("jsonList",jsonList);
 	// Major rows
 	for (var i = 0; i<jsonList.length; i++) {
 	   	//console.log("i", i);
@@ -58,29 +76,26 @@ WindowManager = function (canvas, jsonList) {
 		for (var j = 0; j<jsonList[i].length; j++){
 		    	//console.log("j",j);
 			console.log(jsonList[i][j]);
-			cellsize[0] = canvas.width/this.jsonList[i].length;
-			cellsize[1] = canvas.height/this.jsonList.length;
+			cellsize[0] = canvas.width/jsonList[i].length;
+			cellsize[1] = canvas.height/jsonList.length;
 			win = this.buildWindow(
 				jsonList[i][j],
-				cellsize[0]+this.padding,
-				cellsize[1]+this.padding,
-				cursor[0] - 2*this.padding,
-				cursor[1] - 2*this.padding
+				cellsize[0] - 2*this.padding,
+				cellsize[1] - 2*this.padding,
+				cursor[0] + this.padding,
+				cursor[1] + this.padding
 			);
 			if (win){
 				canvas.dispList.list[0].addChild(win);
-				
-				cursor[0] += cellsize[0];
-				
+								
 			}
-			
+			cursor[0] += cellsize[0];
 		}
 		cursor[0] = 0;
 		cursor[1] += cellsize[1];
 	    
 	}
 }
-
 
 // Constructs Windows from JSON objects
 /*
@@ -96,7 +111,7 @@ WindowManager.prototype.buildWindow = function(windowJSON, width, height, x, y )
 	window = new textWindow(windowJSON.title,windowJSON.text,x,y,width, height);
     } else if (windowJSON.type == "textLine"){
 	console.log("textLine!");
-	window = new textLine(windowJSON.text,x,y,16);
+	window = new textLine(windowJSON.text,x,y,64);
 	window.draggable = true;
     }
 
--- nodeServ.js
+++ nodeServ.js
@@ -6,13 +6,19 @@
 var http = require('http'),
     fs = require('fs'),
     url = require('url'),
-    mongo = require('mongodb'),
+    mongoClient = require('mongodb').MongoClient,
     index;
 var staticResource = require('static-resource');
+
+
 
 var handler = staticResource.createHandler(fs.realpathSync('./'));
 //handler.addContentType('.ttf', 'application/octet-stream');
 
+windows = [
+	[{"type":"textLine", "text":"JTK"},{"type":"textWindow","title":"About This Site" ,"text":"This website is a demonstration of the server-client methodology of JTK"}],
+	[{"type":"textWindow","title":"About JTK" ,"text":"JTK is an experimental canvas application framework. It is built for the Chromium browser and is designed to stress the CPU."},{}],
+];
 
 http.createServer(function (req, res) {
   var qParsed = url.parse(req.url, true);
@@ -22,19 +28,25 @@ http.createServer(function (req, res) {
   //console.log(qData);
   //console.log(req, res);
   var path = url.parse(req.url).pathname;
-  if(!handler.handle(path, req, res)) {
+ console.log("qData", qData);
+ if (qData.jtk){
+	console.log("JSON");	
+	res.writeHead(200, {'Content-Type': 'text/json'});
+	rJSON = JSON.stringify(windows);
+	res.write(rJSON);
+  } else if(!handler.handle(path, req, res)) {
 	console.log(qPath);
 	res.writeHead(404);
 	res.write('404');
 	res.end();
-  } else if (qData.jtkConnection){
-	console.log("JSON");	
-	res.writeHead(200, {'Content-Type': 'text/json'});
-	rJSON = JSON.stringify(["Test Title", qPath, qData]);
-	res.write(rJSON);
   }
   res.end('');
   
 }).listen(1337, '192.168.0.29');
 
 console.log('Server running at http://127.0.0.1:1337/');
+
+/*
+silly test
+[[{"type":"textWindow","title":"Wu-Tang Clan", "text":"Ain't nothing to fuck with!"},{"type":"textWindow","title":"Hello World", "text":"Hut one, hut two, hut three, HUT!"}],[{"type":"textLine", "text":"Old Dirty Bastard"}]]
+*/
